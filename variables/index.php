a6af135cabbaae2580d1f06b34ca91883f2be822
Arbaz Khan <arbazkha78az@gmail.com>
2023-12-05 11:56:06 +0530

    variables is php

---
+++ variables/index.php
@@ -0,0 +1,19 @@
+<!-- 
+    In php Variables are Created with '$'
+    
+    example : $name, $city, $id ;
+
+-->
+
+<?php
+
+//variable initialization
+$name = "arbaz";    //string
+$id = 554;          //number
+$firstLetter = 'a';  //char
+
+echo "name is $name <br>";
+echo "id is $id <br>";
+echo "First Charactor is $firstLetter <br>";
+
+?>
