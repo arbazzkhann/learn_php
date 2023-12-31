2f41289d8dde473d343ca7f916185c2ea867cd00
Arbaz Khan <arbazkha78az@gmail.com>
2023-12-31 13:19:13 +0530

    if else

---
+++ 4. If Else/index.php
@@ -0,0 +1,13 @@
+<!-- 
+    create a program if person age is greater then or equal to 25 or less then or equal to 65 then you can drive.
+ -->
+
+<?php 
+
+$age = 66;
+
+if ($age >= 25 && $age <= 65) {
+    echo "You can drive your age is $age";
+}else 
+    echo "You can not drive your age is $age";
+?>
