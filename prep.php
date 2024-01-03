6062679576b9a3a71c9c25cc506d88b2fde605bf
Arbaz Khan <arbazkha78az@gmail.com>
2024-01-03 12:30:55 +0530

    multidim array prectice

--- prep.php
+++ prep.php
@@ -1,6 +1,19 @@
 <?php 
 
+$multiDim = array(
+    array(24,24,53,42),
+    array(54,44,57,47),
+    array(25,45,92,45),
+    array(62,83,63,44)
+);
 
+for($i=0; $i<count($multiDim); $i++) {
+    for($j=0; $j<count($multiDim[$i]); $j++) {
+        echo $multiDim[$i][$j];
+        echo " ";
+    }
+    echo "<br>";
+}
 
 
 ?>
