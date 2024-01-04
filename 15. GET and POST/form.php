50dcf41c609530ec2decda3a9c2f4df8c59e6992
Arbaz Khan <arbazkha78az@gmail.com>
2024-01-04 10:21:18 +0530

    prectice of get and post

---
+++ 15. GET and POST/form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="prectice.php" method="post">
+        First Name : 
+        <input type="text" name="fname" placeholder="First Name"></input> <br><br>
+        Last Name :
+        <input type="text" name="lname" placeholder="First Name"></input> <br><br>
+        <button type="submit">Submit</button>
+        <button type="reset">Reset</button>
+    </form>
+</body>
+</html>
---
+++ 15. GET and POST/prectice.php
@@ -0,0 +1,8 @@
+<?php 
+
+$fname = $_REQUEST['fname'];
+$lname = $_REQUEST['lname'];
+
+echo "Your name is $fname $lname.";
+
+?>
