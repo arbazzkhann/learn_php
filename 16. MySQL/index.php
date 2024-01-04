4a2836beaa1dd6be9de4401d9443533b3f5027ca
Arbaz Khan <arbazkha78az@gmail.com>
2024-01-04 11:34:47 +0530

    creating connection with db

---
+++ 16. MySQL/index.php
@@ -0,0 +1,33 @@
+<!-- 
+Ways to connect Database to Server 
+    1. MySQLi extension
+    2. PDO
+-->
+
+
+<?php 
+
+//connecting dababase
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+
+// //create a connection
+$conn = mysqli_connect($servername, $username, $password);
+
+//if connection was not stablish
+if(!$conn) {
+    die("Sorry Connection was not stablish" . mysqli_connect_error());
+}
+else {
+    echo "Connection was successfull.";
+}
+
+
+
+
+
+
+
+?>
