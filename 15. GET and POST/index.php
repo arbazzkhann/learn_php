140cb4e3ef068facddee728e26e60cfbada2647e
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2024-01-08 17:27:06 +0530

    inserting comment

--- 15. GET and POST/index.php
+++ 15. GET and POST/index.php
@@ -16,7 +16,7 @@ with POST method data doesn't visibile in url.
   </head>
   <body>
 
-
+<!-- svg -->
   <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
   <symbol id="check-circle-fill" viewBox="0 0 16 16">
     <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
@@ -25,7 +25,7 @@ with POST method data doesn't visibile in url.
 
 
 
-
+<!-- navbar -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">GET / POST</a>
@@ -64,7 +64,7 @@ with POST method data doesn't visibile in url.
 </nav>
 
 
-
+<!-- php code -->
 <?php 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'];
@@ -81,6 +81,8 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 <div class="container mt-3">
 <h1>Enter your email and Password</h1>
 
+
+<!-- form -->
 <form action="index.php" method="post">
   <div class="mb-3">
     <label for="email" class="form-label">Email address</label>
