e4d8cd4d6d6af50e93bb3d434b499fb698d658cd
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2024-01-23 18:40:40 +0530

    2nd commit - connecting db, creating card

--- 33. Project - Online Form/index.php
+++ 33. Project - Online Form/index.php
@@ -1,3 +1,5 @@
+<?php include './partials/_dbconnect.php'?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -7,11 +9,34 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   <body>
-      <?php include './partials/_header.php'?>
-      <?php include './partials/_footer.php'?>
-      
+      <?php include './partials/_header.php'?>    <!--Header-->
+
+      <div class="container my-3">
+        <h2 class="text-center">iDiscuss - Categories</h2>
+        <div class="row">
+
+        <div class="col-md-4">
+            <div class="card" style="width: 18rem;">
+              <img src="https://source.unsplash.com/500x400/?code,python" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>   
+          </div>
+
+          
 
 
+        </div>
+      </div>
+
+
+
+
+
+      <?php include './partials/_footer.php'?>    <!--Footer-->
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
---
+++ 33. Project - Online Form/partials/_dbconnect.php
@@ -0,0 +1,16 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "idiscuss";
+
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if(!$conn) {
+    die ("Error " . mysqli_connect_error());
+}
+
+
+?>
