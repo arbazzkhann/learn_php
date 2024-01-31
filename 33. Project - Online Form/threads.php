0f403963ff8cca172bab46d16078889ae172315b
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2024-01-31 12:37:08 +0530

    creating comments table and importing comments from the database and display on screen.

--- 33. Project - Online Form/threads.php
+++ 33. Project - Online Form/threads.php
@@ -12,9 +12,9 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
         integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <style>
-        #ques{
-            min-height: 500px;
-        }
+    #ques {
+        min-height: 500px;
+    }
     </style>
 </head>
 
@@ -57,48 +57,64 @@
     </div>
 
 
+    <!-- Discussion Form -->
+    <div class="container">
+        <h1 class="mt-5">POST COMMENT</h1>
+        <form action="<?php $_SERVER['REQUEST_URI']?>" method="post">
+            <div class="mb-3">
+                <label for="discussionComment" class="form-label">Type your comment : </label>
+                <textarea rows="6" class="form-control" name="discussion_comment" id="discussionComment"></textarea>
+            </div>
+            <button type="submit" class="btn btn-success">Submit</button>
+        </form>
+    </div>
 
-    <!-- Q & A Section -->
+
+
+
     <div id="ques" class="container my-3 py-4">
 
-    <h1>Discussions</h1>
-    <!-- <?php
-           $cat_id = $_GET['catid'];
+        <h1 class="mb-4">Discussion Section</h1>
+        <?php
+           $thread_id = $_GET['threadid'];
 
            // Corrected SQL query with backticks around table name
-           $sql = "SELECT * FROM `threads` WHERE thread_category_id=$cat_id";
+           $sql = "SELECT * FROM `comments` WHERE thread_id=$thread_id";
            
            $result = mysqli_query($conn, $sql);
-           
-           if (!$result) {
-               // Handle query execution error
-               echo "Error: " . mysqli_error($conn);
-           } else {
+           $noResult = true;
+
                // Fetch data only if the query was successful
                while($row = mysqli_fetch_assoc($result)) {
-                   $thread_title = $row['thread_title'];
-                   $thread_cat_desc = $row['thread_description'];
-
-
+                   $comment_id = $row['comment_id'];
+                   $contant = $row['comment_content'];
+                   $noResult = false;
 
             echo '
                     <div class="media my-3">
                         <img src="./images/userdefault.png" width="40px" alt="...">
                     <div class="media-body">
-                        <h5 class="mt-0"><a href="threads.php">'.$thread_title.'</a></h5>
-                        <p>'.$thread_cat_desc.'</p>
+                        <p>'.$contant.'</p>
                     </div>
                     </div>';
+
                 }
-            }   
+                 
+            if($noResult) {
+                echo '
+                    <div class="jumbotron jumbotron-fluid pl-4">
+                        <h1 class="display-4">NO DISCUSSIONS FOUND</h1>
+                        <p>Be the first person to ask question.</p>
+                    </div>
+                    </div>';
+            }
+            echo '</div>';
+    ?>
 
-    ?> -->
-
-        </div>
 
 
-    <?php include './partials/_footer.php'?>
-    <!--Footer-->
+        <?php include './partials/_footer.php'?>
+        <!--Footer-->
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
